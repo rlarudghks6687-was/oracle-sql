6c56736d167f5f11ddd13034514c4f3e8b228538
gyeonghwan <shade6687@naver.com>
2026-02-02 10:22:09 +0900

    첫 번째 커밋

---
+++ SCOTT.sql
@@ -0,0 +1,9 @@
+-- Oracle(DBMS) - version(21C) -xe(database명)
+-- user(scott) - 테이블.
+-- Structured Query Language (SQL)
+SELECT studno, name -- 칼럼명(전체)
+FROM STUDENT; -- 테이블.
+
+-- 1.professor 테이블. 전체 컬럼 조회.
+SELECT *
+FROM professor;
