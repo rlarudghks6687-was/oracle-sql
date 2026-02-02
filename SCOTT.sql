3dd4fc65661b7b170549fa280698905303b52d8c
gyeonghwan <shade6687@naver.com>
2026-02-02 11:03:25 +0900

    테스트

--- SCOTT.sql
+++ SCOTT.sql
@@ -4,6 +4,11 @@
 SELECT studno, name -- 칼럼명(전체)
 FROM STUDENT; -- 테이블.
 
--- 1.professor 테이블. 전체 컬럼 조회.
+-- 1. professor 테이블. 전체 컬럼 조회.
 SELECT *
+
 FROM professor;
+
+-- 2. student 테이블. 학생번호, 이름, 학년
+SELECT studno, name, grade
+FROM STUDENT;
